31457fa5dd0460c3d789a19f69b49c1888c6d1ab
Niklas Fulle <36989748+Salkin263@users.noreply.github.com>
2023-07-11 23:49:58 +0200

    fifth chapter

--- src/Content.js
+++ src/Content.js
@@ -6,9 +6,25 @@ function Content() {
         const randomIndex = Math.floor(Math.random() * names.length);
         return names[randomIndex];
     };
+
+    const handelClick = () => {
+        console.log("Clicked!");
+    };
+
+    const handelClick2 = (name) => {
+        console.log(`${name}  clicked!`);
+    };
+
+    const handelClick3 = (e) => {
+        console.log(e.target.innerText);
+    };
+
     return (
         <main>
-            <p>Hello {handleNameChange()}!</p>
+            <p onDoubleClick={handelClick}>Hello {handleNameChange()}!</p>
+            <button onClick={handelClick}>Click Me!</button>
+            <button onClick={() => handelClick2("niklas")}>Click Me!</button>
+            <button onClick={(e) => handelClick3(e)}>Click Me!</button>
         </main>
     );
 }
